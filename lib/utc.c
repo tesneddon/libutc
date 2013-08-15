f215cd127c7056100181375e3fa0842b6ba4a245
Tim E. Sneddon <tim@sneddon.id.au>
2013-08-15 10:18:17 +0800

    Add license headers to source modules.

--- lib/utc.c
+++ lib/utc.c
@@ -1,3 +1,45 @@
+/*
+**  FACILITY:   libutc
+**
+**  ABSTRACT:   
+**
+**  MODULE DESCRIPTION:
+**
+**      ...
+**
+**  AUTHOR:     Tim Sneddon <tsneddon@endlesssoftware.com.au>
+**
+**  Copyright (c) 2013, Endless Software Solutions
+**
+**  libutc is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  libutc is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+**
+**  CREATION DATE:   15-AUG-2013
+**
+**  MODIFICATION HISTORY
+**
+**      15-AUG-2013  V1.0    Sneddon    Initial coding.
+**--
+*/
+#include <time.h>
+#include <utc.h>
+
+/*
+**  Forward declarations
+*/
+
+    int utc_gettime(utc_t *utc);
+
 int utc_gettime(utc_t *utc) {
 
     struct timeval tv;
@@ -10,4 +52,3 @@ int utc_gettime(utc_t *utc) {
 
     return utc_mkbintime(utc, &ts, 0, -tz.tz_minuteswest*60);
 } /* utc_gettime */
-
--- lib/utcmk.c
+++ lib/utcmk.c
@@ -1,4 +1,50 @@
 /*
+**  FACILITY:   libutc
+**
+**  ABSTRACT:	UTC make routines
+**
+**  MODULE DESCRIPTION:
+**
+**	This module contains routines that build UTC timestamps from other
+**  sources, like UNIX or OpenVMS system time.
+**
+**  AUTHOR:	Tim Sneddon <tsneddon@endlesssoftware.com.au>
+**
+**  Copyright (c) 2013, Endless Software Solutions
+**
+**  libutc is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  libutc is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+**
+**  CREATION DATE:   15-AUG-2013
+**
+**  MODIFICATION HISTORY
+**
+**      15-AUG-2013  V1.0    Sneddon	Initial coding.
+**--
+*/
+#include <time.h>
+#include <utc.h>
+
+/*
+**  Forward declarations
+*/
+
+    int utc_mkbintime(utc_t *utc, timespec_t *timesp, timespec_t *inaccsp,
+		      long tdf);
+    int utc_mkcomptime(utc_t *utc, UTCValue *timevalue, UTCValue *inaccvalue,
+		       long tdf);
+
+/*
 **++
 **  utc_mkbintime()
 **
@@ -52,7 +98,7 @@ int utc_mkbintime(utc_t *utc,
 
     return utc_mkcomptime(utc, &timevalue, &inaccvalue, tdf);
 } /* utc_mkbintime */
-
+
 /*
  *++
  *  utc_mkcomptime()
